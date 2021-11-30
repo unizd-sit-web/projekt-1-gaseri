7ba6b46ab7197f60d6f81d97b6441a401b9e221e
LukaFilipovic99 <74373893+LukaFilipovic99@users.noreply.github.com>
2021-11-30 19:41:34 +0100

    izmjenjen dizajn

--- Home_page/navbar.js
+++ Home_page/navbar.js
@@ -8,3 +8,10 @@ function scrollFunction() {
     document.getElementById("logo").style.fontSize = "35px";
   }
 }
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.elements.name.value = '';
+  e.target.elements.email.value = '';
+  e.target.elements.message.value = '';
+});
