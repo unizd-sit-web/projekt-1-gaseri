c267ee02d219dc49a1cf680eaaa5c15a89ab86ae
mbuc1c <mbuc1c@users.noreply.github.com>
2021-12-02 19:28:35 +0100

    Uređen nav button

--- Home_page/navbar.js
+++ Home_page/navbar.js
@@ -1,17 +1,1 @@
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementsByClassName("header").style.padding = "30px 10px";
-  } else {
-    document.getElementsByClassName("header").style.padding = "80px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
-  }
-}
-
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  e.target.elements.name.value = '';
-  e.target.elements.email.value = '';
-  e.target.elements.message.value = '';
-});
